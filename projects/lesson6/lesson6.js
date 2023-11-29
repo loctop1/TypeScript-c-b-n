256e513b8296a62152afe13a775ac7dc049876ca
loctop1 <loc2003k22@gmail.com>
2023-11-29 13:43:06 +0700

    part 3: Type Annotations

---
+++ projects/lesson6/lesson6.js
@@ -0,0 +1,11 @@
+// let count = 1;
+
+// count = 'name';
+
+// console.log('>>> check count: ', count)
+
+let names = ['Lộc', 'Khôi', 'Quân']
+
+names.push(25)
+
+console.log('>>> check names: ', names)
---
+++ projects/lesson6/lesson6.ts
@@ -0,0 +1,16 @@
+// Ví dụ về Annotations
+let count2: number;
+
+count2 = 10;
+
+console.log('>>> check count: ', count2)
+
+// let names = ['Lộc', 'Khôi', 'Quân']
+
+// names.push(25)
+
+let names2: string[] = ['Lộc', 'Khôi', 'Quân']
+
+names2.push("25")
+
+console.log('>>> check name 2: ', names2)
