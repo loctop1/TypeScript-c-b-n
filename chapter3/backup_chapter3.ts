15dfce7011f5c93bb1b1e184875f11f6eaa3df54
loctop1 <loc2003k22@gmail.com>
2023-11-30 16:06:07 +0700

    part 21: Do...While

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,11 @@
+// lesson 23 Ví dụ về vòng lặp While Loop
+let counter = 0;
+while (counter < 5) {
+    console.log('Counter = ', counter);
+    if (counter % 2 === 1) break;
+    counter++;
+}
+
 //lesson 22 Ví dụ về vòng lặp For Loop
 for (let i = 0; i < 10; i++) {
     console.log(">> i = ", i);
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,7 +1,12 @@
-// lesson 23 Ví dụ về vòng lặp While Loop
-let counter = 0;
-while (counter < 5) {
-    console.log('Counter = ', counter);
-    if (counter % 2 === 1) break;
-    counter++;
-}
+//lesson 24 Ví dụ về vòng lặp Do While
+
+/**Lặp do-while trong TypeScript tương tự như trong JavaScript. Điều này có nghĩa là khối mã trong do sẽ được thực thi ít nhất 
+ * một lần, sau đó kiểm tra điều kiện trong while. Nếu điều kiện vẫn đúng, nó sẽ tiếp tục lặp. */
+let counter2 = 6
+do {
+    console.log('Counter = ', counter2);
+    if (counter2 % 2 === 1) break;
+    counter2++;
+} while (counter2 < 5);
+/**Điều kiện kiểm tra trong while là counter2 < 5, trong khi bạn đã sử dụng break nếu counter2 % 2 === 1. Do đó, đoạn mã sẽ dừng 
+ * ngay sau lần lặp đầu tiên vì counter2 là số chẵn (6). */
