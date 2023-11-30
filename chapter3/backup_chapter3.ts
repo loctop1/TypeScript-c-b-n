6a8ff1f62d08e228e38802b8cd1bd95cc30f105d
loctop1 <loc2003k22@gmail.com>
2023-11-30 15:33:16 +0700

    part 20: While Loop

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,25 @@
+//lesson 22 Ví dụ về vòng lặp For Loop
+for (let i = 0; i < 10; i++) {
+    console.log(">> i = ", i);
+}
+
+let i1 = 0;
+for (; i1 < 10; i1++) {
+    console.log(i1);
+}
+
+for (let i = 0; ; i++) {
+    console.log(i);
+    if (i > 9) break; // cần có if/break để không khiến vòng lặp chạy vô hạn
+}
+
+let i = 0;
+for (; ;) {
+    console.log(i);
+    i++;
+    if (i > 9) break;
+}
+
 //lesson 21 Ví dụ về switch case
 const age: number = 19;
 switch (age) {
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,21 +1,7 @@
-//lesson 22 Ví dụ về vòng lặp For Loop
-for (let i = 0; i < 10; i++) {
-    console.log(">> i = ", i);
-}
-
-let i1 = 0;
-for (; i1 < 10; i1++) {
-    console.log(i1);
-}
-
-for (let i = 0; ; i++) {
-    console.log(i);
-    if (i > 9) break; // cần có if/break để không khiến vòng lặp chạy vô hạn
-}
-
-let i = 0;
-for (; ;) {
-    console.log(i);
-    i++;
-    if (i > 9) break;
+// lesson 23 Ví dụ về vòng lặp While Loop
+let counter = 0;
+while (counter < 5) {
+    console.log('Counter = ', counter);
+    if (counter % 2 === 1) break;
+    counter++;
 }
