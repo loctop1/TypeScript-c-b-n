ec73ed97ad08a3cba3bb5100ce7df0af5bf2c001
loctop1 <loc2003k22@gmail.com>
2023-11-30 16:47:00 +0700

    part 23: Continue

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,39 @@
+//lesson 25
+//1. Sử dụng TypeScript break để kết thúc vòng lặp
+let products = [
+    { name: 'phone', price: 700 },
+    { name: 'tablet', price: 900 },
+    { name: 'laptop', price: 1200 }
+];
+for (let i = 0; i < products.length; i++) {
+    if (products[i].price == 900)
+        break;
+    console.log(products[i]);
+}
+
+//2. Sử dụng break bên trong switch...case
+let products2 = [
+    { name: 'phone', price: 700 },
+    { name: 'tablet', price: 900 },
+    { name: 'laptop', price: 1200 }
+];
+let discount2 = 0;
+let product = products[1];
+switch (product.name) {
+    case 'phone':
+        discount2 = 5;
+        break;
+    case 'tablet':
+        discount2 = 10;
+    // break;
+    case 'laptop':
+        discount2 = 15;
+    // break;
+    default:
+        discount2 = 100;
+}
+console.log(`There is a ${discount2}% on ${product.name}.`);
+
 //lesson 24 Ví dụ về vòng lặp Do While
 
 /**Lặp do-while trong TypeScript tương tự như trong JavaScript. Điều này có nghĩa là khối mã trong do sẽ được thực thi ít nhất 
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,35 +1,31 @@
-//lesson 25
-//1. Sử dụng TypeScript break để kết thúc vòng lặp
-let products = [
-    { name: 'phone', price: 700 },
-    { name: 'tablet', price: 900 },
-    { name: 'laptop', price: 1200 }
-];
-for (let i = 0; i < products.length; i++) {
-    if (products[i].price == 900)
-        break;
-    console.log(products[i]);
+// lesson 26 Ví dụ về Continue
+
+/**Trong TypeScript (và JavaScript), continue là một từ khóa được sử dụng để bỏ qua phần còn lại của vòng lặp hiện tại và chuyển đến lần lặp tiếp theo.*/
+for (let index = 0; index < 9; index++) {
+    // if index is odd, skip it
+    if (index % 2 === 1)
+        continue;
+    // the following code will be skipped for odd numbers
+    console.log(index);
 }
 
-//2. Sử dụng break bên trong switch...case
-let products2 = [
-    { name: 'phone', price: 700 },
-    { name: 'tablet', price: 900 },
-    { name: 'laptop', price: 1200 }
-];
-let discount2 = 0;
-let product = products[1];
-switch (product.name) {
-    case 'phone':
-        discount2 = 5;
-        break;
-    case 'tablet':
-        discount2 = 10;
-        // break;
-    case 'laptop':
-        discount2 = 15;
-        // break;
-    default:
-        discount2 = 100;
+let index = -1;
+while (index < 9) {
+    index = index + 1;
+    if (index % 2)
+        continue;
+    console.log(index);
 }
-console.log(`There is a ${discount2}% on ${product.name}.`);
+
+let index2 = 9;
+let count = 0;
+do {
+    index2 += 1;
+    if (index2 % 2)
+        continue;
+    count += 1;
+} while (index2 < 99);
+console.log(count); // 45
+/**Trong ví dụ này, vòng lặp sẽ tiếp tục lặp qua các giá trị của index2 từ 10 đến 99. Nếu index2 là số lẻ, nó sẽ sử dụng continue để bỏ qua các bước tiếp theo 
+ * trong vòng lặp và chuyển đến lần lặp mới. Nếu index2 là số chẵn, count sẽ được tăng lên 1. Kết quả cuối cùng in ra giá trị của count sẽ là 45, vì có 45 số chẵn 
+ * từ 10 đến 98 trong khoảng giá trị của index2.*/
