63778f87fe32f873d52ac5539780a21168dfe22f
loctop1 <loc2003k22@gmail.com>
2023-12-01 07:57:30 +0700

    part 24: Function

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,35 @@
+// lesson 26 Ví dụ về Continue
+
+/**Trong TypeScript (và JavaScript), continue là một từ khóa được sử dụng để bỏ qua phần còn lại của vòng lặp hiện tại và chuyển đến lần lặp tiếp theo.*/
+for (let index = 0; index < 9; index++) {
+    // if index is odd, skip it
+    if (index % 2 === 1)
+        continue;
+    // the following code will be skipped for odd numbers
+    console.log(index);
+}
+
+let index = -1;
+while (index < 9) {
+    index = index + 1;
+    if (index % 2)
+        continue;
+    console.log(index);
+}
+
+let index2 = 9;
+let count = 0;
+do {
+    index2 += 1;
+    if (index2 % 2)
+        continue;
+    count += 1;
+} while (index2 < 99);
+console.log(count); // 45
+/**Trong ví dụ này, vòng lặp sẽ tiếp tục lặp qua các giá trị của index2 từ 10 đến 99. Nếu index2 là số lẻ, nó sẽ sử dụng continue để bỏ qua các bước tiếp theo 
+ * trong vòng lặp và chuyển đến lần lặp mới. Nếu index2 là số chẵn, count sẽ được tăng lên 1. Kết quả cuối cùng in ra giá trị của count sẽ là 45, vì có 45 số chẵn 
+ * từ 10 đến 98 trong khoảng giá trị của index2.*/
+
 //lesson 25
 //1. Sử dụng TypeScript break để kết thúc vòng lặp
 let products = [
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,31 +1,11 @@
-// lesson 26 Ví dụ về Continue
-
-/**Trong TypeScript (và JavaScript), continue là một từ khóa được sử dụng để bỏ qua phần còn lại của vòng lặp hiện tại và chuyển đến lần lặp tiếp theo.*/
-for (let index = 0; index < 9; index++) {
-    // if index is odd, skip it
-    if (index % 2 === 1)
-        continue;
-    // the following code will be skipped for odd numbers
-    console.log(index);
+//lesson 27 Ví dụ về Function
+function sum(a: number, b: number) {
+    return a + b
 }
 
-let index = -1;
-while (index < 9) {
-    index = index + 1;
-    if (index % 2)
-        continue;
-    console.log(index);
+//anonymous function
+const sum2 = (a: number, b: number) => {
+    return a + b
 }
 
-let index2 = 9;
-let count = 0;
-do {
-    index2 += 1;
-    if (index2 % 2)
-        continue;
-    count += 1;
-} while (index2 < 99);
-console.log(count); // 45
-/**Trong ví dụ này, vòng lặp sẽ tiếp tục lặp qua các giá trị của index2 từ 10 đến 99. Nếu index2 là số lẻ, nó sẽ sử dụng continue để bỏ qua các bước tiếp theo 
- * trong vòng lặp và chuyển đến lần lặp mới. Nếu index2 là số chẵn, count sẽ được tăng lên 1. Kết quả cuối cùng in ra giá trị của count sẽ là 45, vì có 45 số chẵn 
- * từ 10 đến 98 trong khoảng giá trị của index2.*/
+console.log('>>> check sum: ', sum2(6, 9))
