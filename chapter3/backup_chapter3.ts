22bb30ea3cb0778bcada2b6a3fc8e79845a46875
loctop1 <loc2003k22@gmail.com>
2023-12-02 15:27:15 +0700

    part 33: Readonly

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,47 @@
+//lesson 34 Ví dụ về Access Modifiers ...
+
+/**Access modifiers trong TypeScript là các từ khóa đặt trước các thành phần của một lớp (class) để quy định mức độ truy cập của chúng từ các phạm vi khác nhau. 
+ * TypeScript hỗ trợ ba access modifiers chính: public, private, và protected.
+ * Public (public): Các thành phần có access modifier public có thể truy cập từ bất kỳ nơi nào, không có sự hạn chế.
+ * Private (private): Các thành phần có access modifier private chỉ có thể truy cập bên trong cùng một lớp. Không thể truy cập từ bên ngoài lớp.
+ * Protected (protected): Các thành phần có access modifier protected giống như private, nhưng cũng có thể được truy cập từ các lớp con.*/
+
+//Ví dụ 1: Public và private
+class Employee {
+    public empCode: string;
+    private empName: string;
+    constructor(empCode: string, empName: string) {
+        this.empCode = empCode;
+        this.empName = empName;
+    }
+}
+let emp = new Employee(' abc', ' cde');
+// emp.empCode = "123";
+// emp.empName = "Nguyễn Tuấn Lộc";
+
+console.log(">>> check emp: ", emp)
+
+//Ví dụ 2: Protected
+class Employee1 {
+    public empName: string;
+    public empCode: string;
+    constructor(name: string, code: string) {
+        this.empName = name;
+        this.empCode = code;
+    }
+}
+
+class SalesEmployee extends Employee1 {
+    private department: string;
+    constructor(name: string, code: string, department: string) {
+        super(name, code);
+        this.department = department;
+    }
+}
+let emp1 = new SalesEmployee("John Smith", "123", "Sales");
+emp1.empCode; //Compiler Error
+console.log(">>> check emp1: ", emp1)
+
 //lesson33 Ví dụ về Classes
 
 /**Trong TypeScript, khái niệm "Classes" là một phần quan trọng của hệ thống hướng đối tượng, giúp bạn tổ chức mã nguồn của mình theo cách dễ hiểu và dễ bảo trì. 
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,43 +1,12 @@
-//lesson 34 Ví dụ về Access Modifiers ...
+//lesson 35 Ví dụ về Readonly
 
-/**Access modifiers trong TypeScript là các từ khóa đặt trước các thành phần của một lớp (class) để quy định mức độ truy cập của chúng từ các phạm vi khác nhau. 
- * TypeScript hỗ trợ ba access modifiers chính: public, private, và protected.
- * Public (public): Các thành phần có access modifier public có thể truy cập từ bất kỳ nơi nào, không có sự hạn chế.
- * Private (private): Các thành phần có access modifier private chỉ có thể truy cập bên trong cùng một lớp. Không thể truy cập từ bên ngoài lớp.
- * Protected (protected): Các thành phần có access modifier protected giống như private, nhưng cũng có thể được truy cập từ các lớp con.*/
-
-//Ví dụ 1: Public và private
-class Employee {
-    public empCode: string;
-    private empName: string;
-    constructor(empCode: string, empName: string) {
-        this.empCode = empCode;
-        this.empName = empName;
+/**Trong TypeScript, readonly là một từ khóa được sử dụng để đánh dấu một thành viên của một đối tượng (object) hoặc một phần tử trong một mảng là chỉ đọc. Khi 
+ * một biến hoặc thuộc tính được đánh dấu là readonly, nó chỉ có thể được gán giá trị một lần và không thể thay đổi giá trị sau khi đã được gán. */
+class Person1 {
+    readonly birthDate: Date;
+    constructor(birthDate: Date) {
+        this.birthDate = birthDate;
     }
 }
-let emp = new Employee(' abc', ' cde');
-// emp.empCode = "123";
-// emp.empName = "Nguyễn Tuấn Lộc";
-
-console.log(">>> check emp: ", emp)
-
-//Ví dụ 2: Protected
-class Employee1 {
-    public empName: string;
-    public empCode: string;
-    constructor(name: string, code: string) {
-        this.empName = name;
-        this.empCode = code;
-    }
-}
-
-class SalesEmployee extends Employee1 {
-    private department: string;
-    constructor(name: string, code: string, department: string) {
-        super(name, code);
-        this.department = department;
-    }
-}
-let emp1 = new SalesEmployee("John Smith", "123", "Sales");
-emp1.empCode; //Compiler Error
-console.log(">>> check emp1: ", emp1)
+let person = new Person1(new Date(1990, 12, 25));
+person.birthDate = new Date(1991, 12, 25); // Compile error
