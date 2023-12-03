a5e7216ef4be814bf43723baa035fb632817ed8b
loctop1 <loc2003k22@gmail.com>
2023-12-03 16:28:13 +0700

    part 38: Interfaces

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,46 @@
+//lesson 39 Ví dụ về Abstract Classes
+
+/**Trong TypeScript, Abstract Classes là một khái niệm quen thuộc, giúp bạn tạo ra các lớp cơ sở (base classes) cho các lớp con (derived classes). Một abstract 
+ * class không thể được khởi tạo trực tiếp; thay vào đó, nó được sử dụng để định nghĩa các thuộc tính và phương thức chung cho các lớp con của nó.
+ * Abstract classes giúp bạn xây dựng các kiểu cơ bản (base types) và đảm bảo rằng các lớp con phải cung cấp triển khai cho các phương thức quan trọng. */
+
+abstract class Employee3 {
+    constructor(private firstName: string, private lastName: string) {
+    }
+    abstract getSalary(): number; //abstract method
+    //normal method
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    compensationStatement(): string {
+        return `${this.fullName} makes ${this.getSalary()} a month.`;
+    }
+}
+
+class FullTimeEmployee extends Employee3 {
+    constructor(firstName: string, lastName: string, private salary: number) {
+        super(firstName, lastName); //dùng super để kế thừa lại cha
+    }
+    // cần viết method này, vì nó được khai báo abstract ở trên
+    getSalary(): number {
+        return this.salary;
+    }
+}
+
+class Contractor extends Employee3 {
+    constructor(firstName: string, lastName: string, private rate: number, private hours: number) {
+        super(firstName, lastName);
+    }
+    getSalary(): number {
+        return this.rate * this.hours;
+    }
+}
+
+const test1 = new Contractor("lộc", "Top 1", 1000, 1);
+console.log(">>> test 1 : ", test1.getSalary());
+
 //lesson 38 Ví dụ về Static Methods and Properties
 
 /**Static methods và static properties là những thành phần của một lớp (class) không được gắn liền với các đối tượng của lớp đó, mà thay vào đó liên quan đến 
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,42 +1,22 @@
-//lesson 39 Ví dụ về Abstract Classes
+//lesson 40 Ví dụ về Interfaces
 
-/**Trong TypeScript, Abstract Classes là một khái niệm quen thuộc, giúp bạn tạo ra các lớp cơ sở (base classes) cho các lớp con (derived classes). Một abstract 
- * class không thể được khởi tạo trực tiếp; thay vào đó, nó được sử dụng để định nghĩa các thuộc tính và phương thức chung cho các lớp con của nó.
- * Abstract classes giúp bạn xây dựng các kiểu cơ bản (base types) và đảm bảo rằng các lớp con phải cung cấp triển khai cho các phương thức quan trọng. */
-
-abstract class Employee3 {
-    constructor(private firstName: string, private lastName: string) {
-    }
-    abstract getSalary(): number; //abstract method
-    //normal method
-
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
-
-    compensationStatement(): string {
-        return `${this.fullName} makes ${this.getSalary()} a month.`;
-    }
+/**Trong TypeScript, interface là một cách để định nghĩa các hợp đồng (contracts) cho các đối tượng. Interface mô tả các thành phần công cộng của một đối tượng, 
+ * bao gồm các thuộc tính và phương thức mà đối tượng đó phải có. Điều này giúp kiểm tra kiểu và đảm bảo rằng các đối tượng tuân thủ theo các nguyên tắc được định 
+ * nghĩa.
+ * Interfaces có thể còn được sử dụng để mô tả hình dạng của đối tượng (object shape) mà không cần sử dụng một lớp cụ thể. Bạn cũng có thể sử dụng interfaces để 
+ * mở rộng các đối tượng đã tồn tại. */
+interface IPerson {
+    readonly firstName: string;
+    lastName?: string;
+    address: string;
 }
 
-class FullTimeEmployee extends Employee3 {
-    constructor(firstName: string, lastName: string, private salary: number) {
-        super(firstName, lastName); //dùng super để kế thừa lại cha
-    }
-    // cần viết method này, vì nó được khai báo abstract ở trên
-    getSalary(): number {
-        return this.salary;
-    }
+function getFullName(person: IPerson) {
+    return `${person.firstName} ${person.lastName}`;
 }
-
-class Contractor extends Employee3 {
-    constructor(firstName: string, lastName: string, private rate: number, private hours: number) {
-        super(firstName, lastName);
-    }
-    getSalary(): number {
-        return this.rate * this.hours;
-    }
-}
-
-const test1 = new Contractor("lộc", "Top 1", 1000, 1);
-console.log(">>> test 1 : ", test1.getSalary());
+let person3 = {
+    firstName: 'Eric',
+    lastName: 'Hoi Dan IT',
+    address: 'ha noi'
+};
+console.log(getFullName(person3)); // Eric Hoi Dan IT
