b884e3b4b5377b33c81e3dcf036eb8622d527931
loctop1 <loc2003k22@gmail.com>
2023-12-03 14:51:16 +0700

    part 35: Inheritance

--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,37 +1,39 @@
-//lesson 36 Ví dụ về Getters và Setters
+//lesson 37 Inheritance
 
-/**Trong TypeScript, getters và setters là cách để kiểm soát quyền truy cập và thực hiện các hành động tùy chỉnh khi đọc và ghi giá trị của thuộc tính trong một 
- * lớp.
- * Getter là một phương thức được sử dụng để lấy giá trị của một thuộc tính. Đối với một thuộc tính có getter, bạn sử dụng cú pháp giống như khi truy cập một 
- * thuộc tính thông thường. Điều này giúp ẩn đi triển khai thực tế của thuộc tính và cho phép bạn thực hiện xử lý hoặc tính toán trước khi trả về giá trị.
- * Setter là một phương thức được sử dụng để thiết lập giá trị của một thuộc tính. Đối với một thuộc tính có setter, bạn có thể gán giá trị mới cho nó giống như 
- * khi gán giá trị cho một thuộc tính thông thường. Nhưng trong trường hợp setter, phương thức được gọi và bạn có thể thực hiện các kiểm tra hoặc xử lý trước khi 
- * gán giá trị. */
-class Person2 {
-    public _age: number;
-    public firstName: string;
-    public lastName: string;
-
-    constructor(_age: number, firstName: string, lastName: string) {
-        this._age = _age;
+/**Inheritance (Kế thừa) là một khái niệm trong lập trình hướng đối tượng (OOP) cho phép một lớp (class) mới có thể sử dụng (kế thừa) các thuộc tính và phương 
+ * thức của một lớp hiện có (lớp cha hay lớp cơ sở). Trong TypeScript, bạn sử dụng từ khóa extends để kế thừa từ một lớp khác. */
+class Person3 {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    //getter
-    getAge() {
-        return this._age
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
     }
-    //setter
-    set age(age2: number) {
-        if (age2 < 0 || age2 > 150) {
-            throw Error('Tuổi ko hợp lệ!')
-        }
-        this._age = age2
+    describe(): string {
+        return `This is ${this.firstName} ${this.lastName}.`;
     }
 }
-let person2 = new Person2(25, 'Nguyễn Tuấn Lộc', 'LộcTop1');
-let checkAge = person2.age; //getter
-person2.age = 150; //setter
-console.log(">>> check age: ", person2) //getter
-
-// person2.age = 26;
+//để kế thừa 1 class, chúng ta sử dụng keyword extends
+class Employee2 extends Person3 {
+    private jobTitle;
+    constructor(
+        firstName: string,
+        lastName: string,
+        jobTitle: string) {
+        // call the constructor of the Person class:
+        super(firstName, lastName);
+        this.jobTitle = jobTitle;
+    }
+    //overwrite
+    describe(): string {
+        return (`${super.describe()} - Nguyễn Tuấn Lộc`)
+    }
+}
+//let employee = new Employee('John', 'Doe', 'Front-end Developer');
+//Employee kết thừa lại person => dùng đc method của parent
+let employee = new Employee2('Hoi Dan IT', 'Eric', 'Web Developer');
+console.log(">>> check FullName: ", employee.getFullName());
+console.log(">>> check describe: ", employee.describe());
