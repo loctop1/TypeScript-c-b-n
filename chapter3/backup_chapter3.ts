1bf0a57cdc230d80a0982305d9766d365075abaf
loctop1 <loc2003k22@gmail.com>
2023-12-03 15:19:39 +0700

    part 36: Static Methods and Properties

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,43 @@
+//lesson 37 ví dụ về Inheritance
+
+/**Inheritance (Kế thừa) là một khái niệm trong lập trình hướng đối tượng (OOP) cho phép một lớp (class) mới có thể sử dụng (kế thừa) các thuộc tính và phương 
+ * thức của một lớp hiện có (lớp cha hay lớp cơ sở). Trong TypeScript, bạn sử dụng từ khóa extends để kế thừa từ một lớp khác. */
+class Person3 {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    describe(): string {
+        return `This is ${this.firstName} ${this.lastName}.`;
+    }
+}
+//để kế thừa 1 class, chúng ta sử dụng keyword extends
+class Employee2 extends Person3 {
+    private jobTitle;
+    constructor(
+        firstName: string,
+        lastName: string,
+        jobTitle: string) {
+        // call the constructor of the Person class:
+        super(firstName, lastName);
+        this.jobTitle = jobTitle;
+    }
+    //overwrite
+    describe(): string {
+        return (`${super.describe()} - Nguyễn Tuấn Lộc`)
+    }
+}
+//let employee = new Employee('John', 'Doe', 'Front-end Developer');
+//Employee kết thừa lại person => dùng đc method của parent
+let employee = new Employee2('Hoi Dan IT', 'Eric', 'Web Developer');
+console.log(">>> check FullName: ", employee.getFullName());
+console.log(">>> check describe: ", employee.describe());
+
 //lesson 36 Ví dụ về Getters và Setters
 
 /**Trong TypeScript, getters và setters là cách để kiểm soát quyền truy cập và thực hiện các hành động tùy chỉnh khi đọc và ghi giá trị của thuộc tính trong một 
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,39 +1,18 @@
-//lesson 37 Inheritance
+//lesson 38 Ví dụ về Static Methods and Properties
 
-/**Inheritance (Kế thừa) là một khái niệm trong lập trình hướng đối tượng (OOP) cho phép một lớp (class) mới có thể sử dụng (kế thừa) các thuộc tính và phương 
- * thức của một lớp hiện có (lớp cha hay lớp cơ sở). Trong TypeScript, bạn sử dụng từ khóa extends để kế thừa từ một lớp khác. */
-class Person3 {
-    firstName: string;
-    lastName: string;
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    getFullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
-    describe(): string {
-        return `This is ${this.firstName} ${this.lastName}.`;
+/**Static methods và static properties là những thành phần của một lớp (class) không được gắn liền với các đối tượng của lớp đó, mà thay vào đó liên quan đến 
+ * chính lớp. Điều này có nghĩa là chúng có thể được truy cập mà không cần tạo một đối tượng của lớp đó.
+ * Static properties là các thuộc tính thuộc về lớp chứ không phải đối tượng cụ thể của lớp. Để định nghĩa một static property trong TypeScript, bạn sử dụng từ 
+ * khóa static trước tên thuộc tính.
+ * Static methods là các phương thức thuộc về lớp và không phụ thuộc vào bất kỳ đối tượng cụ thể nào của lớp đó. Để định nghĩa một static method trong TypeScript, 
+ * bạn cũng sử dụng từ khóa static. */
+class Circle {
+    static pi: number = 3.14;
+    public test: number = 69;
+    static calculateArea(radius: number) {
+        return this.pi * radius * radius;
     }
 }
-//để kế thừa 1 class, chúng ta sử dụng keyword extends
-class Employee2 extends Person3 {
-    private jobTitle;
-    constructor(
-        firstName: string,
-        lastName: string,
-        jobTitle: string) {
-        // call the constructor of the Person class:
-        super(firstName, lastName);
-        this.jobTitle = jobTitle;
-    }
-    //overwrite
-    describe(): string {
-        return (`${super.describe()} - Nguyễn Tuấn Lộc`)
-    }
-}
-//let employee = new Employee('John', 'Doe', 'Front-end Developer');
-//Employee kết thừa lại person => dùng đc method của parent
-let employee = new Employee2('Hoi Dan IT', 'Eric', 'Web Developer');
-console.log(">>> check FullName: ", employee.getFullName());
-console.log(">>> check describe: ", employee.describe());
+
+let t = new Circle();
+console.log(">>> check  pi = ", Circle.calculateArea(10))
