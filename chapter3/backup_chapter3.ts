7a268263803f2f9ab905989350c074486364a46d
loctop1 <loc2003k22@gmail.com>
2023-12-03 15:40:28 +0700

    part 37: Abstract Classes

--- chapter3/backup_chapter3.ts
+++ chapter3/backup_chapter3.ts
@@ -1,3 +1,22 @@
+//lesson 38 Ví dụ về Static Methods and Properties
+
+/**Static methods và static properties là những thành phần của một lớp (class) không được gắn liền với các đối tượng của lớp đó, mà thay vào đó liên quan đến 
+ * chính lớp. Điều này có nghĩa là chúng có thể được truy cập mà không cần tạo một đối tượng của lớp đó.
+ * Static properties là các thuộc tính thuộc về lớp chứ không phải đối tượng cụ thể của lớp. Để định nghĩa một static property trong TypeScript, bạn sử dụng từ 
+ * khóa static trước tên thuộc tính.
+ * Static methods là các phương thức thuộc về lớp và không phụ thuộc vào bất kỳ đối tượng cụ thể nào của lớp đó. Để định nghĩa một static method trong TypeScript, 
+ * bạn cũng sử dụng từ khóa static. */
+class Circle {
+    static pi: number = 3.14;
+    public test: number = 69;
+    static calculateArea(radius: number) {
+        return this.pi * radius * radius;
+    }
+}
+
+let t = new Circle();
+console.log(">>> check  pi = ", Circle.calculateArea(10))
+
 //lesson 37 ví dụ về Inheritance
 
 /**Inheritance (Kế thừa) là một khái niệm trong lập trình hướng đối tượng (OOP) cho phép một lớp (class) mới có thể sử dụng (kế thừa) các thuộc tính và phương 
--- chapter3/chapter3.ts
+++ chapter3/chapter3.ts
@@ -1,18 +1,42 @@
-//lesson 38 Ví dụ về Static Methods and Properties
+//lesson 39 Ví dụ về Abstract Classes
 
-/**Static methods và static properties là những thành phần của một lớp (class) không được gắn liền với các đối tượng của lớp đó, mà thay vào đó liên quan đến 
- * chính lớp. Điều này có nghĩa là chúng có thể được truy cập mà không cần tạo một đối tượng của lớp đó.
- * Static properties là các thuộc tính thuộc về lớp chứ không phải đối tượng cụ thể của lớp. Để định nghĩa một static property trong TypeScript, bạn sử dụng từ 
- * khóa static trước tên thuộc tính.
- * Static methods là các phương thức thuộc về lớp và không phụ thuộc vào bất kỳ đối tượng cụ thể nào của lớp đó. Để định nghĩa một static method trong TypeScript, 
- * bạn cũng sử dụng từ khóa static. */
-class Circle {
-    static pi: number = 3.14;
-    public test: number = 69;
-    static calculateArea(radius: number) {
-        return this.pi * radius * radius;
+/**Trong TypeScript, Abstract Classes là một khái niệm quen thuộc, giúp bạn tạo ra các lớp cơ sở (base classes) cho các lớp con (derived classes). Một abstract 
+ * class không thể được khởi tạo trực tiếp; thay vào đó, nó được sử dụng để định nghĩa các thuộc tính và phương thức chung cho các lớp con của nó.
+ * Abstract classes giúp bạn xây dựng các kiểu cơ bản (base types) và đảm bảo rằng các lớp con phải cung cấp triển khai cho các phương thức quan trọng. */
+
+abstract class Employee3 {
+    constructor(private firstName: string, private lastName: string) {
+    }
+    abstract getSalary(): number; //abstract method
+    //normal method
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    compensationStatement(): string {
+        return `${this.fullName} makes ${this.getSalary()} a month.`;
     }
 }
 
-let t = new Circle();
-console.log(">>> check  pi = ", Circle.calculateArea(10))
+class FullTimeEmployee extends Employee3 {
+    constructor(firstName: string, lastName: string, private salary: number) {
+        super(firstName, lastName); //dùng super để kế thừa lại cha
+    }
+    // cần viết method này, vì nó được khai báo abstract ở trên
+    getSalary(): number {
+        return this.salary;
+    }
+}
+
+class Contractor extends Employee3 {
+    constructor(firstName: string, lastName: string, private rate: number, private hours: number) {
+        super(firstName, lastName);
+    }
+    getSalary(): number {
+        return this.rate * this.hours;
+    }
+}
+
+const test1 = new Contractor("lộc", "Top 1", 1000, 1);
+console.log(">>> test 1 : ", test1.getSalary());
